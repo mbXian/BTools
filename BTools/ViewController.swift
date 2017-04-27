8fdde23b51e8bbe22c3acdc1f18d1df018b8a419
Ben <mbxian@xtremeprog.com>
2017-04-27 14:46:58 +0800

    Initial Commit

---
+++ BTools/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BTools
+//
+//  Created by Ben on 27/4/2017.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
